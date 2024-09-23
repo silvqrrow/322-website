ed605f335141c10bb31f8de5b2ecac4942752526
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-09-23 17:37:07 -0400

    Website

---
+++ particles/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import ParticlesComponent from "./components/particles";
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesComponent id="particles" />
+      <div className="container">
+        <h1 className="container__header">Group 26.</h1>
+        <p>Owen, Ghaith, Antony, Yu Chen, Mitchell, & Jasmine</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
