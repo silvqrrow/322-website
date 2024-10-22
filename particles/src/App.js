6de0a23293a4a6438cb1eb499d3c660874dc8c04
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-10-22 14:36:20 -0400

    Update website to include assignment 1 pdf and youtube video

--- particles/src/App.js
+++ particles/src/App.js
@@ -9,6 +9,29 @@ function App() {
         <h1 className="container__header">Group 26.</h1>
         <p>Owen, Ghaith, Antony, Yu Chen, Mitchell, & Jasmine</p>
       </div>
+      <h2>Assignment 1</h2>
+      <div className="assignment-1">
+        <div className="a1-pdf">
+          <iframe
+            src={`${process.env.PUBLIC_URL}/322-A1.pdf`}
+            title="Assignment 1 PDF"
+            width="750"
+            height="500"
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+        <div className="a1-yt">
+          <iframe
+            src="https://www.youtube.com/embed/U15nOaj6m2w"
+            title="Assignment 1 YouTube Video"
+            width="750"
+            height="500"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
