6516b64566e4cd9ada74ccfeae850ac48bbbf23f
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-11-24 17:55:52 -0500

    Add A2 to website

--- particles/src/App.js
+++ particles/src/App.js
@@ -32,6 +32,29 @@ function App() {
           ></iframe>
         </div>
       </div>
+      <h2>Assignment 2</h2>
+      <div className="assignment-1">
+        <div className="a1-pdf">
+          <iframe
+            src={`${process.env.PUBLIC_URL}/322-A2.pdf`}
+            title="Assignment 1 PDF"
+            width="750"
+            height="500"
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+        <div className="a1-yt">
+          <iframe
+            src="https://www.youtube.com/embed/zqS_HAbexnQ"
+            title="Assignment 2 YouTube Video"
+            width="750"
+            height="500"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
